ffd48f645772a247913142070aacd8b7b57fa82b
Pedro Klepa <pedro.klepa@gmail.com>
2020-12-16 18:24:54 +0000

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var compression = require("compression");
+var helmet = require("helmet");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -14,7 +16,8 @@ const KEYS = require("./EXTERNAL_KEYS");
 
 //Set up mongoose connection
 var mongoose = require("mongoose");
-var mongoDB = `mongodb+srv://${KEYS.USER}:${KEYS.PASSWORD}@cluster0.johvh.mongodb.net/local_library?retryWrites=true&w=majority`;
+var dev_db_url = `mongodb+srv://${KEYS.USER}:${KEYS.PASSWORD}@cluster0.johvh.mongodb.net/local_library?retryWrites=true&w=majority`;
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
@@ -23,10 +26,12 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); //Compress all routes
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
